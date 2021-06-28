4a51ec5ae621efdc677150e42f12cc4edcebbdd3
Aswin C <mraswinc@users.noreply.github.com>
2021-06-28 12:10:32 +0800

    initial commit with Teams JS SDK Authentication

---
+++ TeamsTabAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TeamsTabAuth.Startup))]
+namespace TeamsTabAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
